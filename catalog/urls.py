1501a904ea9f5cbb97e7c82172f35416e7542967
MaxDobr <maxim.dobrencko@rambler.ru>
2024-10-10 21:38:41 +0300

    third commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
 from catalog.views import home
+from catalog.views import contacts
 
 app_name = CatalogConfig.name
+
+app_cat = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home')
 
 ]
+
+
